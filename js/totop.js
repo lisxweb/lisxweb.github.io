55bec1b56eb0124e9b8baed5349c014a7700b42b
李双喜 <15110256812@qq.com>
2016-11-17 18:06:47 +0800

    Site updated: 2016-11-17 18:06:47

--- js/totop.js
+++ js/totop.js
@@ -41,17 +41,3 @@ totop.onclick = function() {
   //totop.classList.add('launch');
   return false;
 };
-(function(){
-    console.log(1)
-    var bp = document.createElement('script');
-    var curProtocol = window.location.protocol.split(':')[0];
-    if (curProtocol === 'https') {
-        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
-    }
-    else {
-        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-    }
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(bp, s);
-    console.log(2)
-})();
